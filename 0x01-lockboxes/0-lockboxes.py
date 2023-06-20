bacf17782a059b433c71a8b27567dd0f8bd3e88a
Elijah Sinkamba <scengj@gmail.com>
2023-06-20 13:19:09 +0200

    lockbox

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+  n = len(boxes)
+  visited = [False] * n
+  visited[0] = True
+
+  stack = [0]  # Start with the first box
+
+  while stack:
+      box = stack.pop()
+
+      # Check each key in the current box
+      for key in boxes[box]:
+          if key < n and not visited[key]:
+              visited[key] = True
+              stack.append(key)
+
+  return all(visited)
