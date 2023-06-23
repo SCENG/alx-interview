831c109e70422e36ee6c73d0e5d8fb41fc9e331d
Elijah Sinkamba <scengj@gmail.com>
2023-06-23 07:03:31 +0200

    minimum operation

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Module for 0-minoperations"""
+
+
+def minOperations(n: int) -> int:
+    """
+    Calculates the fewest number of operations needed to result in exactly n H characters.
+
+    Args:
+        n (int): The target number of H characters.
+
+    Returns:
+        int: The minimum number of operations required.
+
+    """
+    if n <= 1:
+        return 0
+
+    operations, divisor = 0, 2
+
+    while n > 1:
+        if n % divisor == 0:
+            n //= divisor
+            operations += divisor
+        else:
+            divisor += 1
+
+    return operations
