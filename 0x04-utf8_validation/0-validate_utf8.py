3ee1a0f9b6f481746350de899509e8d6aac7369e
Elijah Sinkamba <scengj@gmail.com>
2023-06-23 10:10:43 +0200

    correction

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,29 +7,34 @@ def validUTF8(data):
     Method that determines if a given data set represents a valid
     UTF-8 encoding.
     """
-    # Number of bytes in the current UTF-8 character
-    num_bytes = 0
+    number_bytes = 0
 
-    for byte in data:
-        # If the byte starts with 0, it is a single-byte character
-        if num_bytes == 0:
-            # Check if the most significant bit is 0
-            if byte >> 7 == 0b0:
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
+
+    for i in data:
+
+        mask_byte = 1 << 7
+
+        if number_bytes == 0:
+
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            if number_bytes == 0:
                 continue
-            # Check the number of bytes in the current character
-            elif byte >> 5 == 0b110:
-                num_bytes = 1
-            elif byte >> 4 == 0b1110:
-                num_bytes = 2
-            elif byte >> 3 == 0b11110:
-                num_bytes = 3
-            else:
-                return False
-        else:
-            # Check if the byte starts with 10, indicating it is a continuation byte
-            if byte >> 6 != 0b10:
-                return False
-            num_bytes -= 1
 
-    # All characters should have been validated and completed
-    return num_bytes == 0
+            if number_bytes == 1 or number_bytes > 4:
+                return False
+
+        else:
+            if not (i & mask_1 and not (i & mask_2)):
+                    return False
+
+        number_bytes -= 1
+
+    if number_bytes == 0:
+        return True
+
+    return False
