5c8592c41b5c63453b8bd0acb69b5b5bcd26bbcd
Elijah Sinkamba <scengj@gmail.com>
2023-06-23 08:49:20 +0200

    correction

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,42 +2,53 @@
 
 import sys
 
-def print_stats(total_size, status_counts):
-    print("File size: {}".format(total_size))
-    for status, count in sorted(status_counts.items()):
-        print("{}: {}".format(status, count))
 
-def parse_line(line):
-    parts = line.strip().split(" ")
-    if len(parts) != 9:
-        return None, None
-    ip_address = parts[0]
-    status_code = parts[8]
-    try:
-        file_size = int(parts[7])
-    except ValueError:
-        return None, None
-    return ip_address, (status_code, file_size)
+def print_msg(dict_sc, total_file_size):
+    """
+    Method to print
+    Args:
+        dict_sc: dict of status codes
+        total_file_size: total of the file
+    Returns:
+        Nothing
+    """
 
-def compute_metrics():
-    total_size = 0
-    status_counts = {}
+    print("File size: {}".format(total_file_size))
+    for key, val in sorted(dict_sc.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
 
-    try:
-        for i, line in enumerate(sys.stdin, 1):
-            ip_address, data = parse_line(line)
-            if ip_address is None or data is None:
-                continue
-            status_code, file_size = data
 
-            total_size += file_size
-            status_counts[status_code] = status_counts.get(status_code, 0) + 1
+total_file_size = 0
+code = 0
+counter = 0
+dict_sc = {"200": 0,
+           "301": 0,
+           "400": 0,
+           "401": 0,
+           "403": 0,
+           "404": 0,
+           "405": 0,
+           "500": 0}
 
-            if i % 10 == 0:
-                print_stats(total_size, status_counts)
-    except KeyboardInterrupt:
-        pass
+try:
+    for line in sys.stdin:
+        parsed_line = line.split()  # ✄ trimming
+        parsed_line = parsed_line[::-1]  # inverting
 
-    print_stats(total_size, status_counts)
+        if len(parsed_line) > 2:
+            counter += 1
 
-compute_metrics()
+            if counter <= 10:
+                total_file_size += int(parsed_line[0])  # file size
+                code = parsed_line[1]  # status code
+
+                if (code in dict_sc.keys()):
+                    dict_sc[code] += 1
+
+            if (counter == 10):
+                print_msg(dict_sc, total_file_size)
+                counter = 0
+
+finally:
+    print_msg(dict_sc, total_file_size)
